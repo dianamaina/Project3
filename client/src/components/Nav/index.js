be839be3d71e331915a719a0e29d14b53eeb58d4
SBuchanan1 <syd.buch@gmail.com>
2020-11-06 14:51:27 -0500

    changed the wording on the when testing to see if app renders

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        React Reading List
+        Blanket -Access your files on the go!
       </a>
     </nav>
   );
